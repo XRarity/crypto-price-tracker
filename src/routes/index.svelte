04f069bf0b12db104702331e5a611332825b95ef
unknown <egger.laurin@gmail.com>
2021-11-16 19:58:23 +0100

    wip: Setting up the project

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<h1>Crypto Price Tracker</h1>
+
+<ol>
+    <li>Bitcoin 24h - 7Days</li>
+    <li>FTM 24h - 7Days</li>
+    <li>Alert System</li>
+    <li>Calculator - for spending costs - average prices when bought</li>
+</ol>
